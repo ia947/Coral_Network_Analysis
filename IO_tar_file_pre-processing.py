6042b788755135fe383cbd994cea02290cbd3d26
ia947 <ia947@york.ac.uk>
2024-10-30 11:54:46 +0000

    Add lines to extract data from .tar file

--- IO_tar_file_pre-processing.py
+++ IO_tar_file_pre-processing.py
@@ -9,4 +9,7 @@ import numpy as np
 import os
 import tarfile
 
+# Extracting .tar file
 io_tar_file_path = "IOMAtrices.tar.gz"
+with tarfile.open(io_tar_file_path, 'r:gz') as tar:
+    tar.extractall()
