c3c2bcdb604f5c5e3baf4c5b96932ad3b4e9df30
ia947 <ia947@york.ac.uk>
2024-10-31 11:25:19 +0000

    Add pre-processing section and function to read adjacency matrices

--- coral_network_analysis.py
+++ coral_network_analysis.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 17:39:13 2024
+@author: isaac
+"""
 #Import initial modules
 import argparse
 import numpy as np
@@ -6,3 +11,15 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib as plt
 import os
+import tarfile
+
+#################################
+###### DATA PRE-PROCESSING ######
+#################################
+
+
+
+def read_adjacency_matrix(filename):
+    adjacency_matrix = np.load(filename)
+    return adjacency_matrix
+
