dd7f8caa9c07a1be69a070b5d2f85c299b7d9c4e
ia947 <ia947@york.ac.uk>
2024-11-05 14:13:18 +0000

    Add section and function to visualise the adjacency matrices with example data loaded in

--- coral_network_analysis.py
+++ coral_network_analysis.py
@@ -9,9 +9,10 @@ Created on Fri Oct 25 17:39:13 2024
 import argparse
 import numpy as np
 import networkx as nx
+from networkx.drawing.nx_agraph import graphviz_layout
 import pandas as pd
 #import geopandas as gpd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import os
 import tarfile
 
@@ -21,8 +22,10 @@ import tarfile
 
 ## *Make sure all adjacency matrices are in .npy or .csv before reading* ##
 
+filename = r"GBR\tides_only\swain\connectivity_matrix.csv"
+
 def read_adjacency_matrix(filename):
-    adjacency_matrix = np.load(filename)
+    adjacency_matrix = np.genfromtxt(filename, delimiter=',', skip_header=1)
     return adjacency_matrix
 
 # Creating simple graph for the adjacency matrix
@@ -36,4 +39,15 @@ def create_adjacency_matrix_graph(adjacency_matrix):
                 G.add_edge(i,j)
     return G
 
+#################################
+###### DATA VISUALIZATION #######
+#################################
 
+def draw_graph(G):
+    plt.figure(figsize=(10,10))
+    nx.draw(G, with_labels=False, node_color="blue", edge_color="black", node_size=500)
+    plt.show()
+
+adjacency_matrix = read_adjacency_matrix(filename)
+G = create_adjacency_matrix_graph(adjacency_matrix)
+draw_graph(G)
