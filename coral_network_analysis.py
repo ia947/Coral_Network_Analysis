8c1e07d948a2499477b7fbbcd4246fedd559cd8d
ia947 <ia947@york.ac.uk>
2024-11-05 15:10:17 +0000

    Update draw_graph funciton to weight nodes based on eigenvector centrality

--- coral_network_analysis.py
+++ coral_network_analysis.py
@@ -22,7 +22,7 @@ import tarfile
 
 ## *Make sure all adjacency matrices are in .npy or .csv before reading* ##
 
-filename = r"GBR\tides_only\swain\connectivity_matrix.csv"
+filename = r"GBR\wind_and_tides\swain\connectivity_matrix.csv"
 
 def read_adjacency_matrix(filename):
     adjacency_matrix = np.genfromtxt(filename, delimiter=',', skip_header=1)
@@ -30,7 +30,7 @@ def read_adjacency_matrix(filename):
 
 # Creating simple graph for the adjacency matrix
 def create_adjacency_matrix_graph(adjacency_matrix):
-    G = nx.MultiDiGraph()
+    G = nx.DiGraph()
     num_nodes = len(adjacency_matrix)
     G.add_nodes_from(range(num_nodes))
     for i in range(num_nodes):
@@ -43,11 +43,41 @@ def create_adjacency_matrix_graph(adjacency_matrix):
 ###### DATA VISUALIZATION #######
 #################################
 
-def draw_graph(G):
-    plt.figure(figsize=(10,10))
-    nx.draw(G, with_labels=False, node_color="blue", edge_color="black", node_size=500)
+def draw_graph(G, pos=None):
+    if pos is None:
+        pos = nx.spring_layout(G, seed=42, k=0.15)
+
+    # Compute degree centrality
+    eigenvector_centrality = nx.eigenvector_centrality(G, max_iter=1000)
+    
+    centrality_values = list(eigenvector_centrality.values())
+    max_centrality = max(centrality_values)
+    min_centrality = min(centrality_values)
+    
+    # Map the centrality values to a colour scale
+    node_color = [eigenvector_centrality[node] for node in G.nodes()]
+    cmap = plt.cm.get_cmap('coolwarm')
+    
+    node_size = [1000 * eigenvector_centrality[node] for node in G.nodes()]  # Scale node size by centrality
+    edge_color = 'gray'
+    edge_width = 1.5
+    alpha = 0.7 # Edge transparency
+    
+    # Draw graph with nodes coloured based on centrality
+    nx.draw(G, pos, with_labels=False, node_size=node_size, node_color=node_color, cmap=cmap, 
+            edge_color=edge_color, width=edge_width, alpha=alpha, font_weight='bold')
+    
+    sm = plt.cm.ScalarMappable(cmap=plt.cm.plasma, norm=plt.Normalize(vmin=min_centrality, vmax=max_centrality))
+    sm.set_array([])
+    plt.colorbar(sm, label="Eigenvector Centrality")
     plt.show()
 
 adjacency_matrix = read_adjacency_matrix(filename)
 G = create_adjacency_matrix_graph(adjacency_matrix)
 draw_graph(G)
+
+
+
+
+
+
