22dba1af669d87b4c22e24acb4017fa5a3428f3b
Isaac Abbott <ia947@york.ac.uk>
2025-02-04 15:38:48 +0000

    Add section to concat all connectivity csv files into one dataframe, organised by region and condition (i.e. tide only etc for GBR)

--- network_metric_statistical_analysis.py
+++ network_metric_statistical_analysis.py
@@ -9,10 +9,51 @@ import os
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+
+############################################
+###### LOAD CSV FILES FOR EACH REGION ######
+############################################
+
+# Set file paths
+base_path = r"C:\Users\isaac\SynologyDrive\Documents\University of York\BSc (Hons) Environmental Geography\3rd Year (2024-2025)\Dissertation\Code and Data"
+
+# Map all GBR csv files
+GBR_files = [f for f in os.listdir(base_path) if f.startswith("GBR_") and f.endswith("_centrality_measures.csv")]
+
+# Create dictionary with the proper keys
+IO_and_C_centrality_measures_files = {
+    "IO": os.path.join(base_path, "IO_centrality_measures.csv"),
+    "Caribbean": os.path.join(base_path, "Caribbean_centrality_measures.csv"),
+    }
+
+for file in GBR_files:
+    key = file.replace("_centrality_measures.csv", "") # To remove the file suffix
+    IO_and_C_centrality_measures_files[key] = os.path.join(base_path, file)
+    
+# Load all data into dictionary
+dataframes = {region: pd.read_csv(file) for region, file in IO_and_C_centrality_measures_files.items()}
+
+# Then add a region identifier to each dataframe
+for region, df in dataframes.items():
+    # Assign a region name (GBR, IO, or Caribbean)
+    if "GBR" in region:
+        df["Region"] = "GBR"
+        df["Condition"] = region.replace("GBR_", "")  # To store the specific area (e.g., "tides-only_Cairns")
+    elif "IO" in region:
+        df["Region"] = "IO"
+        df["Condition"] = "IO"
+    elif "Caribbean" in region:
+        df["Region"] = "Caribbean"
+        df["Condition"] = "Caribbean"
+
+df_all = pd.concat(dataframes.values(), ignore_index=True)
+
+# Display basic information
+print(df_all.head())
 
