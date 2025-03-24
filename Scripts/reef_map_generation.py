f346965fc60c8e103a0514bd81f3dead736f7f2f
Isaac Abbott <ia947@york.ac.uk>
2025-03-24 12:10:03 +0000

    Import required modules to handle geospatial data with cartopy and geopandas

--- Scripts/reef_map_generation.py
+++ Scripts/reef_map_generation.py
@@ -5,3 +5,10 @@ Created on Mon Mar 24 12:01:41 2025
 @author: isaac
 """
 
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+
